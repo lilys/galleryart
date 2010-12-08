dc0050b33a477a06b9039a2872914bbe9121488e
unknown <Admin@.(none)>
2010-12-07 21:10:47 -0300

    modificacion pagina de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Galleryart::Application.routes.draw do
   resources :disciplines
 
   resources :teachers
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -62,7 +63,7 @@ Galleryart::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
